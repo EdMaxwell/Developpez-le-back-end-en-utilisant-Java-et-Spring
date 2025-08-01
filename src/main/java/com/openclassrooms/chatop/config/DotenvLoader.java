a773e1e99eba315fbe0dad2634cea787be03607c
Pierre TARTROU <pierre.tartrou.pro@gmail.com>
2025-08-01 11:13:27 +0200

    chore: add mysql

---
+++ src/main/java/com/openclassrooms/chatop/config/DotenvLoader.java
@@ -0,0 +1,20 @@
+package com.openclassrooms.chatop.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DotenvLoader {
+
+
+    //TODO: revoir la sécurité du .env
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+            System.out.println("ENV LOADED: " + entry.getKey() + "=" + entry.getValue());
+        });
+    }
+}
